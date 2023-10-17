f98c7a891ba0a7e4b1634eb16af5d707f51330e2
Albert Colubi <albertmc89@gmail.com>
2023-10-17 19:08:12 +0200

    Test PropertiesListPage and modify Header test

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -4,8 +4,8 @@ import Header from "./Header";
 
 describe("Given a Header component", () => {
   describe("When it's rendered", () => {
-    test("Then it should show a 'logo ball with blue and yellor colors'", () => {
-      const headingText = "idealista";
+    test("Then it should show a 'InvestWise'", () => {
+      const headingText = "InvestWise";
 
       render(
         <BrowserRouter>
