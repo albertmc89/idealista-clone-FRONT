db1b9f3b299df6fc9d327ef2e614a467ddaa923d
Albert Colubi <albertmc89@gmail.com>
2023-10-26 21:46:44 +0200

    Dispatch loadProperties in PropertiesListPage

--- src/pages/PropertiesListPage/PropertiesListPage.tsx
+++ src/pages/PropertiesListPage/PropertiesListPage.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import { propertiesMock } from "../../mocks/propertiesMock";
+import { useAppDispatch } from "../../store";
+import { loadPropertiesActionCreator } from "../../store/properties/propertiesSlice";
 import "./PropertiesListPage.css";
 
 const PropertiesListPage = (): React.ReactElement => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(loadPropertiesActionCreator(propertiesMock));
+  }, [dispatch]);
+
   return (
     <>
       <section className="properties-page">
