e020d506ff1fef5d2ced78a962b73af7c313dfad
Albert Colubi <albertmc89@gmail.com>
2024-02-25 19:08:19 +0100

    Test deletePropertyApi

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,22 @@
 import { rest } from "msw";
-import { apiMockProperties } from "./propertiesMock";
+import { apiMockProperties, selectedPropertyMock } from "./propertiesMock";
 
 export const handlers = [
   rest.get(
     `${import.meta.env.VITE_API_PROPERTIES_URL}properties`,
     (_req, res, ctx) => {
       return res(ctx.status(200), ctx.json({ properties: apiMockProperties }));
+    },
+  ),
+  rest.delete(
+    `${import.meta.env.VITE_API_PROPERTIES_URL}properties/${
+      selectedPropertyMock._id
+    }`,
+    (_req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({ message: "Property succesfully deleted" }),
+      );
     },
   ),
 ];
@@ -15,6 +26,14 @@ export const errorHandlers = [
     `${import.meta.env.VITE_API_PROPERTIES_URL}properties`,
     (_req, res, ctx) => {
       return res(ctx.status(404, "Can't get any property"));
+    },
+  ),
+  rest.delete(
+    `${import.meta.env.VITE_API_PROPERTIES_URL}properties/${
+      selectedPropertyMock._id
+    }`,
+    (_req, res, ctx) => {
+      return res(ctx.status(404, "Couldn't delete property"));
     },
   ),
 ];
