2d30e6d74102aebd1d56c94daf19bd19abccc3cb
Albert Colubi <albertmc89@gmail.com>
2024-02-25 14:40:05 +0100

    Dispatch loading actions

--- src/hooks/useInvestmentsApi.ts
+++ src/hooks/useInvestmentsApi.ts
@@ -3,13 +3,20 @@ import axios from "axios";
 import { ApiProperties, Property } from "../types";
 import { auth } from "../firebase";
 import { useIdToken } from "react-firebase-hooks/auth";
+import { useDispatch } from "react-redux";
+import {
+  startLoadingActionCreator,
+  stopLoadingActionCreator,
+} from "../store/ui/uiSlice";
 
 const useInvestmentsApi = () => {
   const [user] = useIdToken(auth);
+  const dispatch = useDispatch();
 
   const apiUrl = import.meta.env.VITE_API_PROPERTIES_URL;
 
   const getProperties = useCallback(async () => {
+    dispatch(startLoadingActionCreator());
     try {
       if (user) {
         const token = await user.getIdToken();
@@ -29,13 +36,15 @@ const useInvestmentsApi = () => {
             id: _id,
           }),
         );
+        dispatch(stopLoadingActionCreator());
 
         return properties;
       }
     } catch {
+      dispatch(stopLoadingActionCreator());
       throw new Error("Can't get any property");
     }
-  }, [apiUrl, user]);
+  }, [apiUrl, user, dispatch]);
 
   return {
     getProperties,
