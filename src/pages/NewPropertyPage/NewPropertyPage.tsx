4b2829457fd16d133b4725adba31cb363b05be12
Albert Colubi <albertmc89@gmail.com>
2024-02-28 16:03:24 +0100

    Add addPropertyApi to custom hook and dispatch

--- src/pages/NewPropertyPage/NewPropertyPage.tsx
+++ src/pages/NewPropertyPage/NewPropertyPage.tsx
@@ -1,26 +1,38 @@
-import NewPlayerForm from "../../components/NewPropertyForm/NewPropertyForm";
+import NewPropertyForm from "../../components/NewPropertyForm/NewPropertyForm";
+import useInvestmentsApi from "../../hooks/useInvestmentsApi";
 import paths from "../../paths/paths";
+import { useAppDispatch } from "../../store";
+import { addPropertyActionCreator } from "../../store/properties/propertiesSlice";
+import { Property } from "../../types";
 import "./NewPropertyPage.css";
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-const NewPlayerPage = (): React.ReactElement => {
+const NewPropertyPage = (): React.ReactElement => {
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const { addPropertyApi } = useInvestmentsApi();
 
   useEffect(() => {
     document.title = "InvestWise | Add a new property";
   }, []);
 
-  const onSubmitProperty = async () => {
+  const onSubmitProperty = async (
+    newproperty: Omit<Property, "id" | "user">,
+  ) => {
+    const property = await addPropertyApi(newproperty);
+
+    dispatch(addPropertyActionCreator(property!));
+
     navigate(paths.properties);
   };
 
   return (
     <div className="addproperty">
       <h2 className="title">Add property</h2>
-      <NewPlayerForm onSubmitProperty={onSubmitProperty} />
+      <NewPropertyForm onSubmitProperty={onSubmitProperty} />
     </div>
   );
 };
 
-export default NewPlayerPage;
+export default NewPropertyPage;
