a1e6e4fb3b0cf60189497cd8d175004fbe5b412c
Albert Colubi <albertmc89@gmail.com>
2024-03-04 12:02:41 +0100

    Test toggleProperty reducer

---
+++ src/store/properties/__tests__/toggleProperty.test.tsx
@@ -0,0 +1,27 @@
+import { propertiesMock } from "../../../mocks/propertiesMock";
+import { PropertyState } from "../../types";
+import {
+  propertiesReducer,
+  togglePropertyActionCreator,
+} from "../propertiesSlice";
+
+describe("Given a properties slice", () => {
+  describe("When it receives a state with a property and a toggleproperty action with id '64fb2a9470bf0a89283a4a88'", () => {
+    test("Then it should return a new state with the property 'isRented' of the property with id '64fb2a9470bf0a89283a4a88' to false", () => {
+      const currentPropertiesState: PropertyState = {
+        properties: propertiesMock,
+      };
+
+      const toggleIsRentedAction = togglePropertyActionCreator({
+        ...propertiesMock[0],
+        isRented: true,
+      });
+      const newPlayersState = propertiesReducer(
+        currentPropertiesState,
+        toggleIsRentedAction,
+      );
+
+      expect(newPlayersState.properties[0]).toHaveProperty("isRented", true);
+    });
+  });
+});
